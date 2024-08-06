6e4f30bd7aa45f0d460e80f361a2e7bfe2df95e9
muneebalam01 <muneebalam232@gmail.com>
2024-08-06 18:28:17 +0500

    Code Pushed To Project One React Js

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+ import './App.css'
+import Hero from './components/Hero'
+import Navigation from './components/Navigation'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+
+   <div>
+    <Navigation />
+    <Hero />
+   </div>
+  )
+}
+
+export default App
